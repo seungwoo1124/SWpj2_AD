a6ce7eac8fdab045051ebc629a708f6c6582a369
seungwoo1124 <dbswlgus9420@naver.com>
2019-12-13 11:09:08 +0900

    our second commit

---
+++ Button.py
@@ -0,0 +1,9 @@
+from PyQt5.QtWidgets import QToolButton
+
+class Button(QToolButton):
+
+    def __init__(self, text, callback):
+        super().__init__()
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        self.setText(text)
+        self.clicked.connect(callback)
---
+++ Engdic.py
@@ -0,0 +1,5 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+
+class Engdic:
+    pass
