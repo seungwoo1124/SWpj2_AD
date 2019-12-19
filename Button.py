2b105e4a1225a45dca500a1c4975fbfeff81de26
seungwoo1124 <dbswlgus9420@naver.com>
2019-12-19 09:18:58 +0900

    final revise

--- Button.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QToolButton
-
-class Button(QToolButton):
-
-    def __init__(self, text, callback):
-        super().__init__()
-        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
-        self.setText(text)
-        self.clicked.connect(callback)
